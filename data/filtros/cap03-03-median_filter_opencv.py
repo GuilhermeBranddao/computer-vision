783deadda4d56957f9bfbdf17822cf6e950cfb0a
GuilhermeBranddao <guilhermebranddao@gmail.com>
2025-07-12 13:03:01 -0300

    up

--- data/filtros/cap03-03-median_filter_opencv.py
+++ data/filtros/cap03-03-median_filter_opencv.py
@@ -13,10 +13,9 @@ args = vars(ap.parse_args())
 image = cv2.imread(args["image"])
 cv2.imshow("Original", image)
 
-# A função cv2.medianBlur é usada principalmente para remover
-# o que se chama ruído "sal e pimenta". Ao contrário da Média
-# método mencionado daqui a pouco com o Gaussian Blur, o método da mediana (como o nome
-# sugere), calcula o valor médio do pixel entre os área ao redor.
+# A função cv2.medianBlur é usada principalmente para remover o que se chama ruído "sal e pimenta". 
+# Ao contrário da Média método mencionado daqui a pouco com o Gaussian Blur, o método da mediana 
+# (como o nome sugere), calcula o valor médio do pixel entre os área ao redor.
 blurred = np.hstack([
 	cv2.medianBlur(image, 3),
 	cv2.medianBlur(image, 5),
